d35434cf3e6bfed3edbc73ccbe7d9ed22662c609
avantgov <alexcouts18@gmail.com>
2021-07-12 16:28:26 -0500

    added some dummy content and framework support for the about page, including adding some router to the app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 // * DEPENDS.
 import './CSS/App.css'
+import { Route } from 'react-router-dom';
 
 // * COMPONENTS
 import Header from './Components/Framework/Header/Header.js'
 import Mission from './Components/Mission/Mission.js'
 import Footer from './Components/Framework/Footer/Footer'
 import ProductList from './Components/Products/ProductList'
+import AboutContent from './Components/About/AboutContent';
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <Mission />
-        <ProductList />
+        <Route exact path='/'>
+          <Mission />
+          <ProductList />
+        </Route>
+
+        <Route exact path='/about'>
+          <AboutContent />
+        </Route>
+        
         <Footer />
     </div>
   );
